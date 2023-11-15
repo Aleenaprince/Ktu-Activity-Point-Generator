72056650ec07cacb683fdea4f5d909e64e57ecad
Aleenaprince <princessaleena2002@gmail.com>
2023-11-15 18:10:18 +0530

    Initial Commit

---
+++ src/components/loginform.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./loginform.css"
+
+const LoginForm = () => {
+    return (
+        <div className="main">
+        <div className="cover">
+        <h1>KTU ACTIVIY POINT GENERATOR</h1>
+        <h2> Calculate your activity Points here in no time..</h2>
+        <div className="Container">
+        <div className="Student">
+        <h1>STUDENT</h1>        
+        </div>
+        <div className="Faculty">
+        <h1> FACULTY</h1>
+        </div>
+        </div>
+        </div>
+        </div>
+      
+    )
+}
+export default LoginForm
