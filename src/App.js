580a9bca4f43b17f6bc77c92014606749c16ff1c
angelmarybinu <98399507+angelmarybinu@users.noreply.github.com>
2023-11-19 18:44:52 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Loginform from "./components/loginform.js"
 import Studentlogin from "./components/studentlogin.js"
 import Facultylogin from "./components/facultylogin.js"
+import Studentdashboard from "./components/studentdashboard.js"
 import {BrowserRouter,Routes,Route}from 'react-router-dom';
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route path="/" element={<Loginform/>}/>
       <Route path='/loginstudent' element={<Studentlogin/>}/>
       <Route path='/loginfaculty' element={<Facultylogin/>}/>
+      <Route path='/studentdashboard' element={<Studentdashboard/>}/>
     </Routes>
     </BrowserRouter>
   );
